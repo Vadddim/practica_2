4b57ba79fe0673ccbc8fa023bf3bacd521f3897e
Вадим <vadimvaddd@gmail.com>
2022-04-05 20:33:27 +0300

    practica2

---
+++ app/src/main/java/com/example/practica2_kornilov/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.practica2_kornilov;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart()");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.i(TAG,"onSaveInstanceState");
+    }
+}
